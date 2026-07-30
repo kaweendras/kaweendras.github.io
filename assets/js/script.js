7d90b9347b1408c28a8e1ae056fb15a78eaf2f3e
kaweendras <salitha.wijerathna@gmail.com>
2026-07-30 13:33:39 +0530

    CHG | print downloadable CV PDF

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,28 @@ projectToggles.forEach((toggle) => {
   });
 });
 
+const cvDownloadLink = document.querySelector('.download-button');
+
 document.querySelectorAll('[data-print]').forEach((button) => {
-  button.addEventListener('click', () => window.print());
+  button.addEventListener('click', () => {
+    if (!cvDownloadLink) {
+      return;
+    }
+
+    const printWindow = window.open('', '_blank');
+
+    if (!printWindow) {
+      window.location.assign(cvDownloadLink.href);
+      return;
+    }
+
+    printWindow.addEventListener('load', () => {
+      window.setTimeout(() => {
+        printWindow.focus();
+        printWindow.print();
+      }, 500);
+    }, { once: true });
+
+    printWindow.location.href = cvDownloadLink.href;
+  });
 });
